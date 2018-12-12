cd94cb69aae7643743f49876bbd15d52ec711dac
fabetroncode <fabetroncode@users.noreply.github.com>
2018-12-12 11:39:03 -0600

    Update Termination_Automation.ps1
    updated modules and changed some loops

--- Termination_Automation.ps1
+++ Termination_Automation.ps1
@@ -1,7 +1,7 @@
 ﻿#--------------------------------------------Disable AD and Email Accounts------------------------------------------------#
-#install azure AD module
-Install-Module -Name AzureADPreview
+#module installs
 Install-Module -Name MSOnline
+Install-Module AzureADPreview
 
 do{ 
 $userName = @()
@@ -11,15 +11,15 @@ $UserCredential = @()
 $Session = @()
 $sessionrestart = @()
 
+
     #Gather username, required input and must not be empty or null
     $userName = (Read-Host -Prompt 'Please input the users User Name.')
 
         #Gather Email, required input and must not be empty or null
         $email = (Read-host -Prompt 'Please enter the users email address, with the domain.')
-  
-  	    #Gather Delegate Email, required input and must not be empty or null
-            $delegateName = (Read-host -Prompt 'Please enter in the email of the person who needs delegate and forward access.')
 
+            #Gather Delegate Email, required input and must not be empty or null
+            $delegateName = (Read-host -Prompt 'Please enter in the email of the person who needs delegate and forward access.')
 
 #####################################Connect to Office 365, office 365 msol service,  and enter in office 365 credentials######################################
       
@@ -33,7 +33,7 @@ $UserCredential = Get-Credential
         Import-PSSession $Session
 
             #Starts MSOL Session with same credentials as the O365 powershell script above. This will be needed when we implement the password-reset api         
-	    Connect-MsolService -Credential $UserCredential    
+	         Connect-MsolService -Credential $UserCredential      
 
                 #Convert Email address to shared mailbox and remove license
                 Set-Mailbox -Identity $email -Type Shared
@@ -50,12 +50,18 @@ $UserCredential = Get-Credential
                                     #prove account is blocked
                                     Get-MsolUser -UserPrincipalName $email | Select DisplayName,BlockCredential
 
-                                        #disables PSsession 
-                                        Remove-PSSession $Session
+                                        #remove licenses from account
+                                        (get-MsolUser -UserPrincipalName $email).licenses.AccountSkuId |
+                                           foreach {
+                                              Set-MsolUserLicense -UserPrincipalName $email -RemoveLicenses $_
+                                                   } 
+
+                                                #disables PSsession 
+                                                Remove-PSSession $Session
                                         
-                                            #restart script check
-                                            $sessionrestart = (Read-Host -prompt "Do you want to start over? Y or N.")
-    } 
+                                                    #restart script check
+                                                    $sessionrestart = (Read-Host -prompt "Do you want to start over? Y or N.")
+            } 
     
         while($sessionrestart -eq "Y") {
 
@@ -63,7 +69,7 @@ $UserCredential = Get-Credential
 
 ###########################################Disconnect from Office 365########################################################################################################
 
-#Disable the User Active Directory Account
+#Disable the User Account
 #Disable-ADAccount -Identity $userName
 
 
