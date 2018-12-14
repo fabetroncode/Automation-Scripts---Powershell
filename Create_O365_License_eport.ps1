9df6579b04299f15b6ada9f923bc81093d366609
fabetroncode <fabetroncode@users.noreply.github.com>
2018-12-14 14:17:19 -0600

    Create Create_O365_License_eport.ps1

---
+++ Create_O365_License_eport.ps1
@@ -0,0 +1,33 @@
+﻿# creates a report of licenses for each client and emails them to the client or Integrity: 
+# Found on https://4sysops.com/archives/manage-office-365-licenses-with-powershell/
+
+
+do{
+$continuePrompt = @()
+
+#enter Office 365 admin credential
+$UserCredential = Get-Credential
+
+    #creates new session in office 365 online
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+                         
+        #imports session 
+        Import-PSSession $Session
+
+$customformat = @{expr={$_.AccountSkuID};label="AccountSkuId"},
+         @{expr={$_.ActiveUnits};label="Total"},
+         @{expr={$_.ConsumedUnits};label="Assigned"},
+        @{expr={$_.activeunits-$_.consumedunits};label="Unassigned"},
+        @{expr={$_.WarningUnits};label="Warning"}
+Get-MsolAccountSku | sort activeunits -desc | select $customformat | Export-CSV "\\its-fp2\users\mfaber\Desktop\Licenses_Report.csv" -NoTypeInformation
+
+Remove-Pssession $Session
+
+$continuePrompt = (Read-host -prompt "Do you want to continue? Y or N")
+ 
+ cls
+
+} while ($continuePrompt -eq "Y") {
+
+}
+
